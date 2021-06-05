385e70742dd22ecd1f653be1fba76c3f87b5d3f5
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-05 16:40:21 -0300

    trocando class por className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Bin2Dec</h1>
-        <p class="lead">Conversor de binário para decimal</p>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Bin2Dec</h1>
+        <p className="lead">Conversor de binário para decimal</p>
       </div>
     </div>
   );
